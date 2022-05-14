96169a19760ab2b6c7cc5f0a25a141c40f8c2cf3
Julia El S <67521112+Juliag875@users.noreply.github.com>
2022-05-14 16:09:08 -0400

    Add files via upload

---
+++ codewars/simpleMultiplication/simpleMultiplication.js
@@ -0,0 +1,11 @@
+//Multiply a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(num) {
+  if (num % 2 == 0) {
+    return num * 8;
+  } else {
+    return num * 9;
+  }
+}
+
+console.log(simpleMultiplication(2))
---
+++ codewars/sumMixedArr/sumMixedArr.js
@@ -0,0 +1,13 @@
+function sumMix(arr) {
+  let sum = 0
+  arr.map((el) => {
+    sum += parseInt(el)
+  })
+  return sum;
+}
+
+const sumMixedOneLine = (arr, sum = 0) => 
+  arr.map(el => parseInt(el)).reduce((sum, el) => sum + el)
+
+console.log(sumMix([1,'2',3]));
+console.log(sumMixedOneLine([1,'2',3]));
