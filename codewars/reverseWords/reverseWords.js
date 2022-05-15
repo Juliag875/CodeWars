7d73ddd051f8ebf6bfdf9fca51a38cca5427e103
Iuliia El Sammak <juliag875@gmail.com>
2022-05-15 19:16:29 -0400

    solved

--- codewars/reverseWords/reverseWords.js
+++ codewars/reverseWords/reverseWords.js
@@ -1,13 +1,9 @@
 //Complete the function that accepts a string parameter, and reverses each word in the string. 
 
 function reverseWords(str) {
-  return str
-    .split('')
-    .reverse()
-    .join('')
-    .split('')
-    .reverse()
-    .join('');
+  return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
+    
 }
 
 console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))
+console.log(reverseWords('apple'))
