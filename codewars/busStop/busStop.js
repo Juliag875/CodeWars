7f3974fc2a8d08666f8d1ddc0498f09e09bbd2f6
Iuliia El Sammak <juliag875@gmail.com>
2022-05-16 14:29:16 -0400

    3rd solution added

--- codewars/busStop/busStop.js
+++ codewars/busStop/busStop.js
@@ -7,13 +7,20 @@ const number = function(busStop) {
   return remaining;
 }
 
-//Solution 2 - reduce() method
+console.log(number([[10,0],[3,5],[5,8]]));
+console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])); 
+
+//Solution 2 - .reduce() method
 function numberReducer(arr) {
   return arr.reduce((sum, i) => sum + (i[0] - i[1]), 0)
 }
 
-
 console.log(numberReducer([[10,0],[3,5],[5,8]]));
 
-console.log(number([[10,0],[3,5],[5,8]]));
-console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])); 
+//Solution 3 - .map() & .reduce()
+function numberRe(arr) {
+  return arr.map(i => i[0] - i[1]).reduce((sum,i) => sum +i)
+}
+
+console.log(numberRe([[10,0],[3,5],[5,8]]));
+console.log(numberRe([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])); 
