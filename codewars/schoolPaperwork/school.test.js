95f336853f052933f3570c57a80d67c096c4a04c
Iuliia El Sammak <juliag875@gmail.com>
2022-05-24 12:38:13 -0400

    solved schoo paperwork

---
+++ codewars/schoolPaperwork/school.test.js
@@ -0,0 +1,13 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Simple Tests", function(){
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(paperwork(5,5), 25, 'Failed at paperwork(5, 5)');
+    assert.strictEqual(paperwork(5,-5), 0, 'Failed at paperwork(5, -5)');
+    assert.strictEqual(paperwork(-5,-5), 0, 'Failed at paperwork(-5, -5)');
+    assert.strictEqual(paperwork(-5,5), 0, 'Failed at paperwork(-5, 5)');
+    assert.strictEqual(paperwork(5,0), 0, 'Failed at paperwork(5, 0)');
+  });
+});
