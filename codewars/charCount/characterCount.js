859f818777481537e76694cf7f46480b8b37bc69
Iuliia El Sammak <juliag875@gmail.com>
2022-06-04 00:38:34 -0400

    test

--- codewars/charCount/characterCount.js
+++ codewars/charCount/characterCount.js
@@ -31,8 +31,8 @@ function charCount(str){
 function isAlphaNumeric(char) {
     let code = char.charCodeAt(0)
     if (!(code > 47 && code < 58) &&  //numeric(0-9)
-        !(code > 64 && code < 91) && //uooer alfa (A-Z)
-        !(code >96 && code < 123)) {
+        !(code > 64 && code < 91) && // alfa (A-Z)
+        !(code > 96 && code < 123)) {
         return false;
         }
     return true
