ef328ec20e08d1174e21875e626426f9ca70e5d1
Iuliia El Sammak <juliag875@gmail.com>
2022-06-15 22:48:13 -0400

    Frequency Count Solution added

--- codewars/areAnagram/areAnagram.js
+++ codewars/areAnagram/areAnagram.js
@@ -5,10 +5,39 @@
 // function('finder', 'Friend')  --> true
 // function('hello', 'bye') --> false 
  
-function areAnagram(str1, str2) {
+function areAnagramOne(str1, str2) {
   return str1.toLowerCase().split('').sort().join('') ===
          str2.toLowerCase().split('').sort().join('')
 }
 
-console.log(areAnagram('listen', 'silent'));
-console.log(areAnagram('they see', 'the eyes')); 
+//Solution Frequency Count - O(n)
+function areAnagramTwo(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const lookup = {}
+  
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i]
+    //if letter exist increment by one otherwise set to 1
+    lookup[letter] ? lookup[letter] +=1 : lookup[letter] = 1
+  }
+  console.log(lookup)
+  for (let i = 0; i < str2.length; i++){
+    let letter = str2[i]
+    //cant find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1
+    }
+  }
+  console.log(lookup)
+  return true;
+}
+
+console.log(areAnagramOne('listen', 'silent'));
+console.log(areAnagramTwo('they see', 'the eyes')); 
+console.log(areAnagramTwo('car', 'rat'));
+console.log(areAnagramTwo('zza', 'aaz'));
