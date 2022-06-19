c6c9bee713e39d8338dc818dc940580be402a43b
Iuliia El Sammak <juliag875@gmail.com>
2022-06-18 21:00:59 -0400

    Power function

---
+++ codewars/recursiveFunctions/power.js
@@ -0,0 +1,13 @@
+//Write a function which accepts a base and exponent. 
+//The fn should rerturn the power of the base to the exponent.
+//This fn should mimic the functionality of Math.pow() - dont worry about negative bases and exponents.
+
+function power(base, exponent){
+  if (exponent===0) return 1;
+  return base * power(base, exponent-1)
+}
+
+
+console.log(power(2,0) )// 1
+console.log(power(2,2)) // 4 
+console.log(power(2,4)) // 16 
