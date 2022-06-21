0838a2a6ffc0e986e8a2b1e4d341987f6dcd406d
Iuliia El Sammak <juliag875@gmail.com>
2022-06-21 00:16:09 -0400

    ES2015 Swap solution

--- codewars/sorting/bubbleSort.js
+++ codewars/sorting/bubbleSort.js
@@ -4,19 +4,37 @@
 //if arr[j] is greater than arr[j+1] => swap those 2 values!
 
 function bubbleSort(arr) {
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j=0; j<arr.length; j++) {
-      console.log(arr)
-      if(arr[j] > arr[j+i]) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i-1; j++) {
+      console.log(arr, arr[j], arr[j+1])
+      if(arr[j] > arr[j+1]) {
         //SWAP
         let temp = arr[j];
-        arr[j] = arr[i]
-        arr[j+1] = temp
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
       }
     }
+    console.log("one pass complete")
   }
   return arr;
 }
 
-console.log(bubbleSort([37,45,29,81]))
+//ES2015 Solution
+function bubbleSortES(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  }
+
+    for (let i = arr.length; i > 0; i--) {
+      for (let j = 0; j < i-1; j++) {
+        if (arr[j] > arr [j+1]) {
+          swap(arr, j, j+1)
+        }
+      }
+    }
+  
+  return arr;
+}
+
+console.log(bubbleSort([37,45,29,8,12,-3,88])) 
+console.log(bubbleSortES([36,44,26,7,11,-5,89])) 
